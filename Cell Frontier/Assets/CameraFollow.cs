50d5902bfb19122c2dd289eaf221c9f4e381a2a0
lkk6057 <lkk6057@gmail.com>
2018-01-27 12:12:20 -0500

    Added player controller and configured camera

---
+++ Cell Frontier/Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Vector3 FollowPos;
+    public GameObject Player;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        FollowPos = new Vector3(Player.transform.position.x,Player.transform.position.y+22,Player.transform.position.z-4); //sets a vector3 to the prefered offset off 
+        transform.position = FollowPos;
+	}
+}
---
+++ Cell Frontier/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Vector3 movement;
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+        Speed = 400f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime*Speed,0,Input.GetAxis("Vertical")*Time.deltaTime*Speed); //sets a Vector3 to built-in WASD axis', applies time delta to keep the speed constant and multiplies it by a speed variable
+        transform.position += movement/100;
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y + 19, transform.position.z - 4);
+        GetComponent<Rigidbody>().AddForce(movement); //applys the force to the player's rigidbody
+	}
+}
