9223bfa7cf82188b147734de43b35f0dd801cbf4
SoulsOfTheVoid <35856633+SoulsOfTheVoid@users.noreply.github.com>
2018-01-28 12:20:35 -0500

    canvas

---
+++ Cell Frontier/Assets/Antibody.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Antibody : MonoBehaviour {
+
+    public GameObject wintime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
