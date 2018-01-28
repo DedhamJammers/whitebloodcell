7da393776eddadc963a7cc0bee135dcb4323bd52
SoulsOfTheVoid <35856633+SoulsOfTheVoid@users.noreply.github.com>
2018-01-28 10:54:46 -0500

    update

--- Cell Frontier/Assets/damage.cs
+++ Cell Frontier/Assets/damage.cs
@@ -9,7 +9,7 @@ public class Damage : MonoBehaviour
     void Update()
     {
         healthbar.transform.localScale = new Vector3(Mathf.Clamp(value: currentHealth / 100f, min: 0, max: 1), y: 1, z: 1);
-        if (GameObject.FindGameObjectsWithTag(tag: "Virus Count").Length >= 25)
+        if (GameObject.FindGameObjectsWithTag(tag: "Virus Count").Length >= 150)
         {
             currentHealth -= 5 * Time.deltaTime;
         }
